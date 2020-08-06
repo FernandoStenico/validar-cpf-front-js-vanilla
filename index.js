cbde25bfb0c81b69c3bb0d5d7df7bb366ce07c7a
fernandostenico <fer.elias1@gmail.com>
2020-08-06 14:54:22 -0300

    suporte

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL="https://itau-turma6a-stenico.herokuapp.com/"
+const URL="https://itau-turma6a-stenico.herokuapp.com//validacpf?cpf="
 function validar(){
 
 	var txtCPF = document.getElementById("txtCpf").value;
